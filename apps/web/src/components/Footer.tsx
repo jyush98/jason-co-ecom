81fc464206d77967a4f343bcfcc79017b770cc19
jyush98 <55372314+jyush98@users.noreply.github.com>
2025-06-09 16:49:10 -0400

    Clean up footer

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -7,8 +7,8 @@ import { FaInstagram, FaTiktok, FaEnvelope } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-12  w-full">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-start md:items-center">
-        <div className="flex flex-col md:flex-row gap-12">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col items-center justify-center text-center">
+        {/* <div className="flex flex-col md:flex-row gap-12">
           <div>
             <h5 className="text-lg font-semibold mb-2">Quick Links</h5>
             <ul className="space-y-2 text-sm">
@@ -19,7 +19,7 @@ export default function Footer() {
               <li><Link href="/contact" className="hover:underline">Contact</Link></li>
             </ul>
           </div>
-          <div>
+          <div className="hidden">
             <h5 className="text-lg font-semibold mb-2">Connect</h5>
             <ul className="space-y-2 text-sm">
               <li><a href="#" className="hover:underline">Instagram</a></li>
@@ -27,13 +27,13 @@ export default function Footer() {
               <li><a href="#" className="hover:underline">Email Us</a></li>
             </ul>
           </div>
-        </div>
-        <div className="mt-6 md:mt-0 text-center">
+        </div> */}
+        <div className="mt-6 md:mt-0 text-center flex flex-col items-center justify-center">
           <Image src="/logo.jpg" alt="Jason & Co." width={250} height={125} className="object-contain mx-auto" />
           <div className="mt-2 flex justify-center space-x-6 text-xl">
-            <a href="#" aria-label="Instagram" className="hover:text-gray-300"><FaInstagram /></a>
-            <a href="#" aria-label="TikTok" className="hover:text-gray-300"><FaTiktok /></a>
-            <a href="#" aria-label="Email" className="hover:text-gray-300"><FaEnvelope /></a>
+            <a href="https://www.instagram.com/jasonjeweler/" target="_blank" aria-label="Instagram" className="hover:text-gray-300"><FaInstagram /></a>
+            <a href="https://www.tiktok.com/@jasonjeweler?_t=ZP-8x4FKyhxJKZ&_r=1" target="_blank" aria-label="TikTok" className="hover:text-gray-300"><FaTiktok /></a>
+            <a href="mailto:jonathan@jasonjewels.com" aria-label="Email" className="hover:text-gray-300"><FaEnvelope /></a>
           </div>
         </div>
       </div>
